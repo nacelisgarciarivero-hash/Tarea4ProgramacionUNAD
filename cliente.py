de53cf688661555499bbe8d4b14b065a4abc4260
nacelisgarciarivero-hash <nacelisgarciarivero@gmail.com>
2026-05-05 21:12:19 -0500

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -1,1 +1,17 @@
-# Clase Cliente
+class Cliente:
+    def __init__(self, nombre, correo, telefono):
+        if not nombre:
+            raise ValueError("El nombre no puede estar vacío")
+
+        if "@" not in correo:
+            raise ValueError("Correo inválido")
+
+        if not telefono.isdigit():
+            raise ValueError("Teléfono inválido")
+
+        self.__nombre = nombre
+        self.__correo = correo
+        self.__telefono = telefono
+
+    def mostrar(self):
+        return f"Cliente: {self.__nombre}, Correo: {self.__correo}, Tel: {self.__telefono}"
